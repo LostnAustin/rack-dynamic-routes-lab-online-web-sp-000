a09610e4a523aad12b65cc0d443ef07fff10fa79
Chad Lengefeld <chadlengefeld@gmail.com>
2020-03-23 21:47:57 +0000

    Automatically backed up by Learn

---
+++ app/application.rb
@@ -0,0 +1,25 @@
+require 'pry'
+
+class Application
+
+  def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
+    # binding.pry
+    if req.path=="/items/Figs"
+      #  binding.pry
+      resp.write @@items[0].price
+
+    elsif req.path=="/items/Pears"
+      resp.write @@items[1].price
+
+    elsif req.path!="/items"
+      resp.write "Item not found"
+      resp.status = 400
+
+     resp.write "Route not found"
+      resp.status = 404
+    end
+    resp.finish
+  end
+end
