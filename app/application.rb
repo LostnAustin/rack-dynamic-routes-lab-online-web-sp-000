15324a9960ab6f8a1ca5b473c0f3767c37aa32a5
Chad Lengefeld <chadlengefeld@gmail.com>
2020-03-23 22:14:43 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -16,7 +16,7 @@ class Application
     elsif req.path!="/items/"
       resp.write "Item not found"
       resp.status = 400
-    else
+      
      resp.write "Route not found"
       resp.status = 404
     end
