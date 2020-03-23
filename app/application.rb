cd5f9a880dde221b89a7075519cfe89087343dc6
Chad Lengefeld <chadlengefeld@gmail.com>
2020-03-23 22:34:16 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -12,12 +12,12 @@ class Application
 
     elsif req.path=="/items/Pears"
       resp.write @@items[1].price
+    # 
+    # elsif req.path!="/items/"
+    #   resp.write "Item not found"
+    #   resp.status = 400
 
-    elsif req.path!="/items/"
-      resp.write "Item not found"
-      resp.status = 400
-
-      
+  else  
      resp.write "Route not found"
       resp.status = 404
     end
