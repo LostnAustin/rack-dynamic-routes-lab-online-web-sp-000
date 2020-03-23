697a538746467665f34ce829b8e16a60ee84992c
Chad Lengefeld <chadlengefeld@gmail.com>
2020-03-23 22:05:00 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -13,7 +13,7 @@ class Application
     elsif req.path=="/items/Pears"
       resp.write @@items[1].price
 
-    elsif req.path!="/items"
+    elsif req.path!="/items/"
       resp.write "Item not found"
       resp.status = 400
 
